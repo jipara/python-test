da29d86a5075752fbbddbea8f8b0bff6cde2f86d
jipara <50920745+jipara@users.noreply.github.com>
2022-05-04 09:31:49 -0500

    Create multiple-function-loop.py

---
+++ lesson-6/multiple-function-loop.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+
+for i in range(6):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
