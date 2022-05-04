3e84369e85787e1b31f981dbbee7e15332b4168d
jipara <50920745+jipara@users.noreply.github.com>
2022-05-04 12:58:57 -0500

    Update multiple-function-loop.py

--- lesson-6/multiple-function-loop.py
+++ lesson-6/multiple-function-loop.py
@@ -23,3 +23,24 @@ for i in range(6):
 ##########
 #while not at_goal():
    # jump()
+
+#########
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
