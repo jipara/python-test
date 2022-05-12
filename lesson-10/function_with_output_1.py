6b08de8ad3ca805a6d79329140017c1e8b074a99
jipara <50920745+jipara@users.noreply.github.com>
2022-05-12 12:16:27 -0500

    Create function_with_output_1.py

---
+++ lesson-10/function_with_output_1.py
@@ -0,0 +1,8 @@
+def format_name(first,last):
+  name = first.title()
+  # print(name)
+  last_name = last.title()
+  # print(last_name )
+  # print("name " + first,last)
+  return f"{name} {last_name}"
+print(format_name("jipara", "esen"))
