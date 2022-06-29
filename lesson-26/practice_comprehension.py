796915586b12436d3af83663b498fb1882742f5b
jipara <50920745+jipara@users.noreply.github.com>
2022-06-29 14:54:40 -0500

    Update practice_comprehension.py

--- lesson-26/practice_comprehension.py
+++ lesson-26/practice_comprehension.py
@@ -10,3 +10,18 @@ result = [num for num in numbers if num % 2 == 0]
 #Write your code 👆 above:
 
 print(result)
+
+
+####
+
+with open("file1.txt") as file1:
+  list_file1 = file1.readlines()
+
+with open("file2.txt") as file2:
+  list_file2 = file2.readlines()  
+
+result = [int(n.strip()) for n in list_file1 if n in list_file2]
+
+# Write your code above 👆
+
+print(result)
