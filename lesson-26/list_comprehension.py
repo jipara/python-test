8c06c52eefb5bfc36682d8d6e852c199a460a1c1
jipara <50920745+jipara@users.noreply.github.com>
2022-06-29 13:55:17 -0500

    Create list_comprehension.py

---
+++ lesson-26/list_comprehension.py
@@ -0,0 +1,13 @@
+numbers = [1, 2 ,3]
+#new_numbers = [new_item for item in list]
+new_numbers = [n + 1 for n in numbers]
+
+name = "Jipara"
+new = [letter for letter in name]
+
+new_range = [n * 2 for n in range(1, 5)]
+
+names = ["Aziz", "David", "Jipara", "Kamila", "Zayn"]
+short_names = [name for name in names if len(name) < 5]
+
+short_names = [name.upper() for name in names if len(name) < 5]
