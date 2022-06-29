8df73f58ace6d0b6fc7d26720c6deb1b2c86fb01
jipara <50920745+jipara@users.noreply.github.com>
2022-06-29 14:23:59 -0500

    Update practice_comprehension.py

--- lesson-26/practice_comprehension.py
+++ lesson-26/practice_comprehension.py
@@ -4,6 +4,8 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 #Write your 1 line code 👇 below:
 
 result = [num for num in numbers if not num % 2]
+#or
+result = [num for num in numbers if num % 2 == 0]
 
 #Write your code 👆 above:
 
