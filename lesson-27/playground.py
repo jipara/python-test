8e2b86c0fd958cbf4d6cc98f08bf4045adaf47e9
jipara <50920745+jipara@users.noreply.github.com>
2022-06-30 15:04:45 -0500

    Update playground.py

--- lesson-27/playground.py
+++ lesson-27/playground.py
@@ -6,3 +6,31 @@ def add(*args):
     return sum
 
 print(add(2, 3, 6))
+
+
+## **kwargs
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+        #print(value)
+    #print(kwargs["add"])
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+calculate(2, add=3, multiply=2)
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+        self.make = kwargs["make"]
+        self.model = kwargs["model"]
+
+        #or
+
+        self.make = kwargs.get("make")
+        self.make = kwargs.get("model")
+
+my_car = Car(make="Toyota", model="Camry")
+print(my_car.model)
